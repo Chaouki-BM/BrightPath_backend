43b201d68bee46b45f0cc362ca807d71cc789fe3
Chaouki-BM <Chaouki.BenMiled@gmail.com>
2025-06-01 20:54:29 +0100

    a5er commit nchallh XD

--- src/Models/SeanceDirect.model.js
+++ src/Models/SeanceDirect.model.js
@@ -5,7 +5,6 @@ const seanceDirectSchema = new mongoose.Schema({
   date: String,
   heure: String,
   link:{ type: Date, default: null },
-  
   cours: { type: mongoose.Schema.Types.ObjectId, ref: 'Cours' }
 },{
     timestamps:true
